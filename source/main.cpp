f033abf2963db8553f84f1bc47f80aeda04377e8
Alejandro <alejandro.suau@gmail.com>
2025-11-17 11:25:11 +0100

    Lecture 2: OpenGL init

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -6,6 +7,10 @@ int main() {
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     GLFWwindow* window = glfwCreateWindow(1200, 720, "GameEngine3D", nullptr, nullptr);
     if (!window) {
         std::cout << "Error creating window" << std::endl;
@@ -13,7 +18,20 @@ int main() {
         return -1;
     }
 
+    glfwSetWindowPos(window, 750, 150);
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        glfwTerminate();
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.f, 0.f, 0.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
