b651183b5042b53e430162484312bce6b9dfd73f
Alejandro <alejandro.suau@gmail.com>
2026-01-21 20:07:51 +0100

    Lecture 24 - Set Parent Implementation

--- engine/source/scene/Scene.cpp
+++ engine/source/scene/Scene.cpp
@@ -20,7 +20,7 @@ void Scene::Clear() {
     m_objects.clear();
 }
     
-GameObject* Scene::CreateObject(const std::string& name, GameObject* parent = nullptr) {
+GameObject* Scene::CreateObject(const std::string& name, GameObject* parent) {
     auto obj = new GameObject();
     obj->SetName(name);
     SetParent(obj, parent);
@@ -29,7 +29,106 @@ GameObject* Scene::CreateObject(const std::string& name, GameObject* parent = nu
 }
 
 bool Scene::SetParent(GameObject* obj, GameObject* parent) {
+    bool result = false;
+    auto currentParent = obj->GetParent();
 
+    if (parent) {
+        if (currentParent) {
+            auto it = std::find_if(
+                currentParent->m_children.begin(),
+                currentParent->m_children.end(),
+                [obj](const std::unique_ptr<GameObject>& el) {
+                    return el.get() == obj;
+                });
+            
+            if (it != currentParent->m_children.end()) {
+                bool found = false;
+                auto currentElement = parent;
+                while (currentElement) {
+                    if (currentElement == obj) {
+                        found = true;
+                        break;
+                    }
+                    currentElement = currentElement->GetParent();
+                }
+
+                if (!found) {
+                    parent->m_children.push_back(std::move(*it));
+                    obj->m_parent = parent;
+                    currentParent->m_children.erase(it);
+                    result = true;
+                }
+            }
+        } else {
+            // No parent currently. This can be in 2 cases
+            // 1. Object is in the scene root
+            // 2. The object has been just created
+            auto it = std::find_if(
+                m_objects.begin(),
+                m_objects.end(),
+                [obj](const std::unique_ptr<GameObject>& el) {
+                    return el.get() == obj;
+                });
+            
+            if (it == m_objects.end()) {
+                std::unique_ptr<GameObject> objHolder(obj);
+                parent->m_children.push_back(std::move(objHolder));
+                obj->m_parent = parent;
+                result = true;
+            } else {
+                bool found = false;
+                auto currentElement = parent;
+                while (currentElement) {
+                    if (currentElement == obj) {
+                        found = true;
+                        break;
+                    }
+                    currentElement = currentElement->GetParent();
+                }
+
+                if (!found) {
+                    parent->m_children.push_back(std::move(*it));
+                    obj->m_parent = parent;
+                    m_objects.erase(it);
+                    result = true;
+                }
+            }
+        }
+    } else {
+        if (currentParent) {
+            auto it = std::find_if(
+                currentParent->m_children.begin(),
+                currentParent->m_children.end(),
+                [obj](const std::unique_ptr<GameObject>& el) {
+                    return el.get() == obj;
+                });
+
+            if (it != currentParent->m_children.end()) {
+                m_objects.push_back(std::move(*it));
+                obj->m_parent = nullptr;
+                currentParent->m_children.erase(it);
+                result = true;
+            }
+        } else {
+            // No parent currently. This can be in 2 cases
+            // 1. Object is in the scene root
+            // 2. The object has been just created
+            auto it = std::find_if(
+                m_objects.begin(),
+                m_objects.end(),
+                [obj](const std::unique_ptr<GameObject>& el) {
+                    return el.get() == obj;
+                });
+            
+            if (it == m_objects.end()) {
+                std::unique_ptr<GameObject> objHolder(obj);
+                m_objects.push_back(std::move(objHolder));
+                result = true;
+            }
+        }
+    }
+
+    return result;
 }
 
 }
